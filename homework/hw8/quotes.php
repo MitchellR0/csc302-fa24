c1ef893c059e762be84640cd61659251b8103ef9
Mitchell Romani <mromani@mail.endicott.edu>
2024-09-29 23:23:43 -0400

    php is sending correct vaules, have to finish html and error handling.

--- homework/hw8/quotes.php
+++ homework/hw8/quotes.php
@@ -1,14 +1,19 @@
 <?php
+
     $fullList = file('quotes.csv');
-    $fullListCSV = str_getcsv($fullList);
+    $fullListCSV = [];
+
+    foreach ($fullList as $line){
+        array_push($fullListCSV, str_getcsv($line));
+    }
 
     $n = $_GET['n'];
     
     $quotes = [];
-    for($i = 0; $i <= $n; $i++){
-        $quotes = [
-            'author' => $fullListCSV[i][0],
-            'quote' => $fullListCSV[i][1]
+    for($i = 1; $i <= $n; $i++){
+        $quotes[] = [
+            'author' => $fullListCSV[$i][0],
+            'quote' => $fullListCSV[$i][1]
         ];
     }
 
